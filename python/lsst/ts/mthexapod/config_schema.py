38b5cc0615ea707541c34fdc8a7debb15a769f38
Russell Owen <rowen@uw.edu>
2021-05-17 11:26:06 -0700

    CONFIG_SCHEMA: add min_compensation_adjustment

--- python/lsst/ts/mthexapod/config_schema.py
+++ python/lsst/ts/mthexapod/config_schema.py
@@ -42,9 +42,13 @@ definitions:
         description: >-
           Elevation compensation coefficients.
           Rows are coefficients for x, y, z (um), u, v, w (deg).
-          Values are the coefficients in equation C0 + C1 el + C2 el^2 + ..., where el is in deg.
-          Compensated value = uncompensated (user-specified) value + elevation compensation
-            + azimuth compensation + rotation compensation + temperature compensation.
+          Values are the coefficients in equation C0 + C1 el + C2 el^2 + ...,
+          where el is in deg.
+          Compensated value = uncompensated (user-specified) value
+            + elevation compensation
+            + azimuth compensation
+            + rotation compensation
+            + temperature compensation.
         type: array
         minItems: 6
         maxItems: 6
@@ -57,7 +61,8 @@ definitions:
         description: >-
           Azimuth compensation coefficients.
           Rows are coefficients for x, y, z (um), u, v, w (deg).
-          Values are the coefficients in equation C0 + C1 az + C2 az^2 + ..., where az is in deg.
+          Values are the coefficients in equation C0 + C1 az + C2 az^2 + ...,
+          where az is in deg.
         type: array
         minItems: 6
         maxItems: 6
@@ -70,7 +75,8 @@ definitions:
         description: >-
           Camera rotation compensation coefficients.
           Rows are coefficients for x, y, z (um), u, v, w (deg).
-          Values are the coefficients in equation C0 + C1 rot + C2 rot^2 + ..., where rot is in deg.
+          Values are the coefficients in equation C0 + C1 rot + C2 rot^2 + ...,
+          where rot is in deg.
         type: array
         minItems: 6
         maxItems: 6
@@ -83,7 +89,8 @@ definitions:
         description: >-
           Temperature compensation coefficients.
           Rows are coefficients for x, y, z (um), u, v, w (deg).
-          Values are the coefficients in equation C0 + C1 temp + C2 temp^2 + ..., where temp is in C.
+          Values are the coefficients in equation C0 + C1 temp + C2 temp^2 + ...,
+          where temp is in C.
         type: array
         minItems: 6
         maxItems: 6
@@ -92,21 +99,35 @@ definitions:
           minItems: 1
           items:
             type: number
+      min_compensation_adjustment:
+        description: >-
+          The smallest compensation adjustment the CSC's background compensation
+          task will command: x, y, z (um), u, v, w (deg).
+          See the User Guide for details.
+        type: array
+        minItems: 6
+        maxitems: 6
+        items:
+          type: number
+          minimum: 0
       min_temperature:
         description: >-
           Minimum temperatures (C) for which the temperature model is valid.
-          Below this temperature, terms above the first order are ignored; see RangedPolynomial for details.
+          Below this temperature, terms above the first order are ignored;
+          see RangedPolynomial for details.
         type: number
       max_temperature:
         description: >-
           Maximum temperatures (C) for which the temperature model is valid.
-          Above this temperature, terms above the first order are ignored; see RangedPolynomial for details.
+          Above this temperature, terms above the first order are ignored;
+          see RangedPolynomial for details.
         type: number
     required:
       - elevation_coeffs
       - azimuth_coeffs
       - rotation_coeffs
       - temperature_coeffs
+      - min_compensation_adjustment
       - min_temperature
       - max_temperature
     additionalProperties: false
@@ -148,6 +169,13 @@ properties:
         - [0]
         - [0]
         - [0]
+      min_compensation_adjustment:
+        - 1
+        - 1
+        - 1
+        - 1.0e-4
+        - 1.0e-4
+        - 1.0e-4
       min_temperature: -20
       max_temperature: 30
   m2_config:
@@ -181,6 +209,13 @@ properties:
         - [0]
         - [0]
         - [0]
+      min_compensation_adjustment:
+        - 1
+        - 1
+        - 1
+        - 1.0e-4
+        - 1.0e-4
+        - 1.0e-4
       min_temperature: -20
       max_temperature: 30
 required: [camera_config, m2_config]
