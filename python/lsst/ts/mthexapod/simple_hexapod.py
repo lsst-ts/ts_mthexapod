c95b091c4311b562c13b286f48d25e4cb249b665
Te-Wei <twtsaia@gmail.com>
2025-03-10 13:40:55 -0300

    Translate the Simulink calculation.

--- python/lsst/ts/mthexapod/simple_hexapod.py
+++ python/lsst/ts/mthexapod/simple_hexapod.py
@@ -25,8 +25,10 @@ import typing
 import numpy as np
 import numpy.typing
 from lsst.ts import simactuators
+from scipy.optimize import minimize
 
 from . import utils
+from .constants import NUM_STRUT
 
 
 class SimpleHexapod:
@@ -68,14 +70,14 @@ class SimpleHexapod:
         max_length: float,
         speed: float,
     ) -> None:
-        if len(base_positions) != 6:
+        if len(base_positions) != NUM_STRUT:
             raise ValueError(f"base_positions={base_positions} must have 6 elements")
         for pos in base_positions:
             if len(pos) != 3:
                 raise ValueError(
                     f"base_positions={base_positions}; each item must have 3 elements"
                 )
-        if len(mirror_positions) != 6:
+        if len(mirror_positions) != NUM_STRUT:
             raise ValueError(
                 f"mirror_positions={mirror_positions} must have 6 elements"
             )
@@ -86,17 +88,13 @@ class SimpleHexapod:
                 )
         if len(pivot) != 3:
             raise ValueError(f"pivot={pivot} must have 3 elements")
-        self.base_positions = [np.array(pos) for pos in base_positions]
+        self.base_positions = np.array(base_positions).T * utils.UM_TO_M
         # Positions of mirror actuator ends and pivot at orientation zero.
-        self.neutral_mirror_positions = [np.array(pos) for pos in mirror_positions]
-        self.neutral_pivot = np.array(pivot, dtype=float)
+        self.neutral_mirror_positions = np.array(mirror_positions).T * utils.UM_TO_M
+        self.neutral_pivot = np.array(pivot, dtype=float) * utils.UM_TO_M
         # Information commanded by the `move` command.
         self.cmd_pos = np.zeros(3, dtype=float)
         self.cmd_xyzrot = np.zeros(3, dtype=float)
-        self.cmd_mirror_positions = [np.array(pos) for pos in mirror_positions]
-        self.neutral_actuator_lengths = self.compute_actuator_lengths(
-            mirror_positions=mirror_positions, absolute=True
-        )
         self.actuators = [
             simactuators.PointToPointActuator(
                 min_position=min_length,
@@ -104,7 +102,7 @@ class SimpleHexapod:
                 start_position=0,
                 speed=speed,
             )
-            for actuator_length in self.neutral_actuator_lengths
+            for idx in range(NUM_STRUT)
         ]
 
     @classmethod
@@ -206,12 +204,20 @@ class SimpleHexapod:
         duration : `float`
             Duration of the move (second).
         """
-        mirror_positions = self.compute_mirror_positions(pos=pos, xyzrot=xyzrot)
-        # print(f"mirror_positions={mirror_positions}")
-        # print(f"neutral_mirror_positions={self.neutral_mirror_positions}")
-        actuator_lengths = self.compute_actuator_lengths(
-            mirror_positions=mirror_positions, absolute=False
+
+        positions_xyz = np.array(pos) * utils.UM_TO_M
+        positions_rxryrz = np.deg2rad(np.array(xyzrot))
+
+        actuator_lengths = (
+            SimpleHexapod.inverse_kinematics(
+                np.append(positions_xyz, positions_rxryrz),
+                self.neutral_pivot,
+                self.neutral_mirror_positions,
+                self.base_positions,
+            )
+            / utils.UM_TO_M
         )
+
         self.assert_in_range(actuator_lengths)
         max_duration = 0
         for actuator, actuator_length in zip(self.actuators, actuator_lengths):
@@ -219,7 +225,6 @@ class SimpleHexapod:
             max_duration = max(duration, max_duration)
         self.cmd_pos = np.array(pos)
         self.cmd_xyzrot = np.array(xyzrot)
-        self.cmd_mirror_positions = mirror_positions
         return max_duration
 
     def stop(self) -> None:
@@ -240,80 +245,142 @@ class SimpleHexapod:
         if not all(in_range):
             raise ValueError(f"One or more actuators would be out of range: {in_range}")
 
-    def compute_actuator_lengths(
-        self, mirror_positions: list[numpy.typing.NDArray[np.float64]], absolute: bool
+    @staticmethod
+    def inverse_kinematics(
+        positions: numpy.typing.NDArray[np.float64],
+        pivot: numpy.typing.NDArray[np.float64],
+        mirror_positions: numpy.typing.NDArray[np.float64],
+        base_positions: numpy.typing.NDArray[np.float64],
     ) -> numpy.typing.NDArray[np.float64]:
-        """Compute actuator lengths, given mirror positions.
+        """Inverse kinematics for a hexapod movement. This calculates the delta
+        strut lengths based on the hexapod positions.
+
+        Notes
+        -----
+        The calculation is translated from ts_mt_hexRot_simulink repository:
+        hexapod_controller_source_final/hexapod_kin_calc.slx
 
         Parameters
         ----------
-        mirror_positions : `list` [`numpy.ndarray`]
-            Position of the mirror end of each actuator.
-        absolute : `bool`
-            If True then return end to end actuator lengths.
-            If False then return lengths relative to neutral lengths;
-            this requires ``self.neutral_actuator_lengths``.
+        positions : `numpy.ndarray`
+            Hexapod positions: (x, y, z, rx, ry, rz). The units are the meter
+            and radian.
+        pivot : `numpy.ndarray`
+            Pivot (x, y, z) as the rotation center. The unit is meter.
+        mirror_positions : `numpy.ndarray`
+            Strut positions on the mirror. This is a 3x6 matrix. The row is the
+            (x, y, z) position in meter. The column is the strut index.
+        base_positions : `numpy.ndarray`
+            Strut positions on the base. This is a 3x6 matrix. The row is the
+            (x, y, z) position in meter. The column is the strut index.
 
         Returns
         -------
-        lengths : `numpy.ndarray`
-            End to end length of each actuator.
+        `numpy.ndarray`
+            6 delta strut lengths in meter.
         """
-        lengths = np.array(
-            [
-                np.linalg.norm(mirror_pos - base_pos)
-                for mirror_pos, base_pos in zip(mirror_positions, self.base_positions)
-            ],
-            dtype=float,
+
+        x, y, z, rx, ry, rz = positions
+
+        # Consider the translation part
+        translation = np.kron(
+            (np.array([x, y, z]) + pivot).reshape(len(pivot), -1),
+            np.ones(NUM_STRUT),
         )
-        if not absolute:
-            lengths -= self.neutral_actuator_lengths
-        return lengths
 
-    def compute_mirror_positions(
-        self, pos: tuple[float, float, float], xyzrot: tuple[float, float, float]
-    ) -> list[numpy.typing.NDArray[np.float64]]:
-        """Compute the actuator mirror positions needed to move the pivot point
-        to a specified orientation.
+        # Consider the rotation part
+
+        # Rotation matrix
+        rot_x = np.array(
+            [[1, 0, 0], [0, np.cos(rx), -np.sin(rx)], [0, np.sin(rx), np.cos(rx)]]
+        )
+        rot_y = np.array(
+            [[np.cos(ry), 0, np.sin(ry)], [0, 1, 0], [-np.sin(ry), 0, np.cos(ry)]]
+        )
+        rot_z = np.array(
+            [[np.cos(rz), -np.sin(rz), 0], [np.sin(rz), np.cos(rz), 0], [0, 0, 1]]
+        )
+        rot_xyz = rot_x.dot(rot_y).dot(rot_z)
+
+        # Rotate the base positions
+        rotation_projection = rot_xyz.dot(
+            base_positions
+            - np.kron(
+                pivot.reshape(len(pivot), -1),
+                np.ones(NUM_STRUT),
+            )
+        )
+
+        # Delta change
+        delta_location = translation + rotation_projection - mirror_positions
+
+        # Calculate the delta strut lengths
+        strut_length_total = np.sqrt(np.sum(delta_location**2, axis=0))
+
+        strut_length_norm = np.sqrt(
+            np.sum((base_positions - mirror_positions) ** 2, axis=0)
+        )
+        strut_length_delta = strut_length_total - strut_length_norm
+
+        return strut_length_delta
+
+    @staticmethod
+    def forward_kinematics(
+        initial_guess: numpy.typing.NDArray[np.float64],
+        strut_length_delta: numpy.typing.NDArray[np.float64],
+        pivot: numpy.typing.NDArray[np.float64],
+        mirror_positions: numpy.typing.NDArray[np.float64],
+        base_positions: numpy.typing.NDArray[np.float64],
+        tol: float = 1e-6,
+    ) -> numpy.typing.NDArray[np.float64]:
+        """Forward kinematics for a hexapod movement. This calculates the
+        hexapod positions based on the delta strut lengths.
+
+        Notes
+        -----
+        The calculation is translated from ts_mt_hexRot_simulink repository:
+        hexapod_controller_source_final/hexapod_kin_calc.slx
 
         Parameters
         ----------
-        pos : `tuple`
-            Desired x, y, z position of pivot point,
-            relative to the neutral pivot point.
-        xyzrot : `tuple`
-            Orientation of translated pivot point,
-            as a rotation about x, then y, then z (deg).
+        initial_guess : `numpy.ndarray`
+            Initial guess of the hexapod positions: (x, y, z, rx, ry, rz). The
+            units are the meter and radian.
+        strut_length_delta : `numpy.ndarray`
+            6 delta strut lengths in meter.
+        pivot : `numpy.ndarray`
+            Pivot (x, y, z) as the rotation center. The unit is meter.
+        mirror_positions : `numpy.ndarray`
+            Strut positions on the mirror. This is a 3x6 matrix. The row is the
+            (x, y, z) position in meter. The column is the strut index.
+        base_positions : `numpy.ndarray`
+            Strut positions on the base. This is a 3x6 matrix. The row is the
+            (x, y, z) position in meter. The column is the strut index.
+        tol : `float`, optional
+            Tolerance for the optimization. (the default is 1e-6)
 
         Returns
         -------
-        mirror_positions : `list` [`numpy.ndarray`]
-            Resulting position of the mirror end of each actuator.
+        `numpy.ndarray`
+            Estimated hexapod positions: (x, y, z, rx, ry, rz). The units are
+            the meter and radian.
         """
 
-        xyzrot_rad = np.array(xyzrot, dtype=float) * utils.RAD_PER_DEG
-        # Compute the mirror position and resulting length of each actuator,
-        # as well as determining if that length in range.
-        mirror_positions = []
-        for neutral_mirror_pos, base_pos in zip(
-            self.neutral_mirror_positions, self.base_positions
-        ):
-            # Rotate actuator mirror position
-            # (we could do this after translation, but before is a bit easier).
-            mirror_pos_in_pivot_frame = neutral_mirror_pos - self.neutral_pivot
-            mirror_pos_in_pivot_frame = utils.rot_about_x(
-                mirror_pos_in_pivot_frame, xyzrot_rad[0]
-            )
-            mirror_pos_in_pivot_frame = utils.rot_about_y(
-                mirror_pos_in_pivot_frame, xyzrot_rad[1]
-            )
-            mirror_pos_in_pivot_frame = utils.rot_about_z(
-                mirror_pos_in_pivot_frame, xyzrot_rad[2]
-            )
-            mirror_pos = mirror_pos_in_pivot_frame + self.neutral_pivot
-
-            # Translate actuator mirror position
-            mirror_pos = mirror_pos + np.array(pos, dtype=float)
-            mirror_positions.append(mirror_pos)
-
-        return mirror_positions
+        res = minimize(
+            lambda x: np.sum(
+                (
+                    SimpleHexapod.inverse_kinematics(
+                        x,
+                        pivot,
+                        mirror_positions,
+                        base_positions,
+                    )
+                    - strut_length_delta
+                )
+                ** 2
+            ),
+            initial_guess,
+            method="Nelder-Mead",
+            tol=tol,
+        )
+        return res.x
