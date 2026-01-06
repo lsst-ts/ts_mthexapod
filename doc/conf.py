6f5776a3e24de3324c8f6b8e3e0e2b3ce83495f0
Te-Wei Tsai <twtsaia@gmail.com>
2026-01-06 16:38:54 -0700

    Add the documenteer.toml.

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,7 @@ This configuration only affects single-package Sphinx documentation builds.
 """
 
 import lsst.ts.mthexapod  # noqa
-from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
+from documenteer.conf.guide import *  # type: ignore # noqa
 
 project = "ts_mthexapod"
 html_theme_options["logotext"] = project  # type: ignore # noqa
